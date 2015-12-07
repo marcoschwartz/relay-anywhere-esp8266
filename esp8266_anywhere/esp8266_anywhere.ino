f7a1a53fec3a8a4760ff4bb67eaa03e51340dd15
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-12-07 08:19:37 +0100

    initial commit

---
+++ esp8266_anywhere/esp8266_anywhere.ino
@@ -0,0 +1,63 @@
+// Control ESP8266 anywhere
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <aREST.h>
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Create aREST instance
+aREST rest = aREST(client);
+
+// Unique ID to identify the device for cloud.arest.io
+char* device_id = "e49n2di";
+
+// WiFi parameters
+const char* ssid = "yourWiFiNetwork";
+const char* password = "yourWiFiNetwork";
+
+// Functions
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Set callback
+  client.setCallback(callback);
+  
+  // Give name and ID to device
+  rest.set_id(device_id);
+  rest.set_name("relay_anywhere");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set output topic
+  char* out_topic = rest.get_topic();
+
+}
+
+void loop() {
+
+  // Connect to the cloud
+  rest.loop(client);
+
+}
+
+// Handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  rest.handle_callback(client, topic, payload, length);
+
+}
